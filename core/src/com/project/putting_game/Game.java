1e57db172761fb53c468c31bab7c0bd492454166
Mavrokefalidis <lgubm23@gmail.com>
2018-03-20 12:20:16 +0100

    Altered bucket game so it looks closer to the putting game.

--- core/src/com/project/putting_game/Game.java
+++ core/src/com/project/putting_game/Game.java
@@ -7,21 +7,16 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.TimeUtils;
-import com.badlogic.gdx.utils.Array;
-
-import java.util.Iterator;
 
 public class Game extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private OrthographicCamera camera;
-	private Texture bucketImage;
-	private Texture dropImage;
-	private Rectangle bucket;
-	private Array<Rectangle> raindrops;
+	private Texture ballImage;
+	private Texture fieldTexture;
+	private Rectangle ball;
+	private Rectangle field;
 	private long lastDropTime;
 	
 	@Override
@@ -33,19 +28,24 @@ public class Game extends ApplicationAdapter {
 		batch = new SpriteBatch();
 
 		//load images of droplet and bucket
-        dropImage = new Texture(Gdx.files.internal("droplet.png"));
-        bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+        ballImage = new Texture(Gdx.files.internal("golfball.png"));
+        fieldTexture = new Texture(Gdx.files.internal("green.png"));
 
         //Create bucket Rectangle
-        bucket = new Rectangle();
-        bucket.x = 800/2-64/2;
-        bucket.y = 20;
-        bucket.width = 64;
-        bucket.height = 64;
+        ball = new Rectangle();
+        ball.x = 800/2-64/2;
+        ball.y = 20;
+        ball.width = 64;
+        ball.height = 64;
 
-        //Make it rain
-        raindrops = new Array<Rectangle>();
-        spawnRaindrop();
+        //Create field
+        field = new Rectangle();
+        field.x = 60;
+        field.y = 60;
+        field.width = 800 - 60*2;
+        field.height = 480 - 60*2;
+
+
 
 	}
 
@@ -57,10 +57,9 @@ public class Game extends ApplicationAdapter {
 
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(bucketImage, bucket.x, bucket.y);
-		for(Rectangle raindrop: raindrops) {
-		    batch.draw(dropImage, raindrop.x, raindrop.y);
-        }
+        batch.draw(fieldTexture, field.x, field.y, field.width, field.height);
+		batch.draw(ballImage, ball.x, ball.y, 64, 64);
+
 		batch.end();
 
 		//Makes clicking or touching to move bucket possible
@@ -68,47 +67,28 @@ public class Game extends ApplicationAdapter {
             Vector3 touchPos = new Vector3();
             touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             camera.unproject(touchPos);
-            bucket.x = touchPos.x-64/2;
-            bucket.y = touchPos.y-20;
+            ball.x = touchPos.x-64/2;
+            ball.y = touchPos.y-20;
         }
 
         //Makes using clicks to move bucket possible
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 400 * Gdx.graphics.getDeltaTime();
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 400 * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) ball.x -= 400 * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) ball.x += 400 * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)) ball.y += 400 * Gdx.graphics.getDeltaTime();
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) ball.y -= 400 * Gdx.graphics.getDeltaTime();
 
         //Makes sure the bucket doesn't get out of the window
-        if(bucket.x < 0) bucket.x = 0;
-        if(bucket.x > 800 - 64) bucket.x = 800 - 64;
+        if(ball.x < 60) ball.x = 60;
+        if(ball.x > 800 - 124) ball.x = 800 - 124;
+        if(ball.y < 60) ball.y = 60;
+        if(ball.y > 480 - 124) ball.y = 480-124;
 
-        //How often to spawn raindrops
-        if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
-
-        //Make the rain fall at a certain speed
-        Iterator<Rectangle> iter = raindrops.iterator();
-        while(iter.hasNext()) {
-            Rectangle raindrop = iter.next();
-            raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
-            if(raindrop.y + 64 < 0) iter.remove();
-            if(raindrop.overlaps(bucket)) {
-                iter.remove();
-            }
-        }
 	}
 	
 	@Override
 	public void dispose () {
-	    dropImage.dispose();
-	    bucketImage.dispose();
+        ballImage.dispose();
 		batch.dispose();
 	}
 
-	private void spawnRaindrop() {
-	    Rectangle raindrop = new Rectangle();
-	    raindrop.x = MathUtils.random(0, 800-64);
-	    raindrop.y = 480;
-	    raindrop.width = 64;
-	    raindrop.height = 64;
-	    raindrops.add(raindrop);
-	    lastDropTime = TimeUtils.nanoTime();
-    }
 }
