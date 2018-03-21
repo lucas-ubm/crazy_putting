7f589caef24d4bc19580b33bd6d78b9017d45923
Mavrokefalidis <lgubm23@gmail.com>
2018-03-21 15:22:40 +0100

    Now ball moves like it should move on game, still have to tweak some thing

--- core/src/com/project/putting_game/Game.java
+++ core/src/com/project/putting_game/Game.java
@@ -2,7 +2,6 @@ package com.project.putting_game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -17,7 +16,6 @@ public class Game extends ApplicationAdapter {
 	private Texture fieldTexture;
 	private Rectangle ball;
 	private Rectangle field;
-	private long lastDropTime;
 	
 	@Override
 	public void create () {
@@ -33,8 +31,8 @@ public class Game extends ApplicationAdapter {
 
         //Create bucket Rectangle
         ball = new Rectangle();
-        ball.x = 800/2-64/2;
-        ball.y = 20;
+        ball.x = 90 + 64;
+        ball.y = 64;
         ball.width = 64;
         ball.height = 64;
 
@@ -62,32 +60,71 @@ public class Game extends ApplicationAdapter {
 
 		batch.end();
 
-		//Makes clicking or touching to move bucket possible
+        Vector3 origin = new Vector3();
+        Vector3 ballPos = new Vector3();
+        boolean condition = false;
 		if(Gdx.input.isTouched()) {
             Vector3 touchPos = new Vector3();
             touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
             camera.unproject(touchPos);
-            ball.x = touchPos.x-64/2;
-            ball.y = touchPos.y-20;
+            origin.set((int)touchPos.x - 64/2, (int)touchPos.y - 64/2, 0);
+
+            condition = true;
+            ballPos.set((int)ball.x, (int)ball.y, 0);
+
+            Vector3 destination = new Vector3();
+            Vector3 direction = new Vector3();
+            direction.set((ballPos.x-origin.x), (ballPos.y-origin.y), 0);
+            destination.set(ballPos.add(direction));
+            direction.setLength(1);
+
+            while(Math.abs(ball.x - destination.x) > 10 || Math.abs(ball.y - destination.y) > 10) {
+                System.out.println("Direction is " + direction + " ball was in" + ballPos + " clicked in " +origin+ " ball is in " + ball.x + " " + ball.y);
+
+                if(ball.x < destination.x) {
+                    ball.x += 0.1*direction.x * Gdx.graphics.getDeltaTime();
+                }
+                if(ball.x > destination.x) {
+                    ball.x -= 0.1*direction.x * Gdx.graphics.getDeltaTime();
+                }
+
+                if(ball.y < destination.y) {
+                    ball.y += 0.1*direction.y * Gdx.graphics.getDeltaTime();
+                }
+                if(ball.y > destination.y){
+                    ball.y -= 0.1*direction.y * Gdx.graphics.getDeltaTime();
+                }
+            }
         }
 
-        //Makes using clicks to move bucket possible
-        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) ball.x -= 400 * Gdx.graphics.getDeltaTime();
-        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) ball.x += 400 * Gdx.graphics.getDeltaTime();
-        if(Gdx.input.isKeyPressed(Input.Keys.UP)) ball.y += 400 * Gdx.graphics.getDeltaTime();
-        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) ball.y -= 400 * Gdx.graphics.getDeltaTime();
-
+//		//Makes clicking or touching to move bucket possible
+//		if(Gdx.input.isTouched()) {
+//            Vector3 touchPos = new Vector3();
+//            touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+//            camera.unproject(touchPos);
+//            ball.x = touchPos.x-64/2;
+//            ball.y = touchPos.y-20;
+//        }
+//
+//        //Makes using clicks to move bucket possible
+//        if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) ball.x -= 400 * Gdx.graphics.getDeltaTime();
+//        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) ball.x += 400 * Gdx.graphics.getDeltaTime();
+//        if(Gdx.input.isKeyPressed(Input.Keys.UP)) ball.y += 400 * Gdx.graphics.getDeltaTime();
+//        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) ball.y -= 400 * Gdx.graphics.getDeltaTime();
+//
         //Makes sure the bucket doesn't get out of the window
         if(ball.x < 60) ball.x = 60;
         if(ball.x > 800 - 124) ball.x = 800 - 124;
         if(ball.y < 60) ball.y = 60;
         if(ball.y > 480 - 124) ball.y = 480-124;
 
+
 	}
 	
 	@Override
 	public void dispose () {
         ballImage.dispose();
+        fieldTexture.dispose();
 		batch.dispose();
 	}
 
