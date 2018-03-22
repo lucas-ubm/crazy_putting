02cb3911d2885717a431b09eea918cdb01f9409d
Mavrokefalidis <lgubm23@gmail.com>
2018-03-22 13:04:48 +0100

    Created Field and Properties classes (sorry for modifying master directly)

---
+++ core/src/com/project/putting_game/Field.java
@@ -0,0 +1,29 @@
+package com.project.putting_game;
+
+import com.badlogic.gdx.math.Vector3;
+
+public class Field {
+    public Properties[][] matrix;
+    public Vector3 hole;
+    public int holeRadius;
+
+    public Field (int x, int y, Vector3 hole, int holeRadius) {
+        matrix = new Properties[y][x];
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[].length; j++) {
+                matrix[i][j].height = Math.sin(i) + Math.pow(j,2);
+            }
+        }
+
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[].length; j++) {
+                matrix[i][j].friction = Math.sin(i) + Math.cos(Math.sin(Math.pow(j,2)));
+            }
+        }
+
+        this.hole = hole;
+        this.holeRadius = holeRadius;
+
+    }
+}
---
+++ core/src/com/project/putting_game/Properties.java
@@ -0,0 +1,12 @@
+package com.project.putting_game;
+
+public class Properties {
+    public double height;
+    public double friction;
+
+    public Properties(double height, double friction) {
+        this.height = height;
+        this.friction = friction;
+    }
+
+}
