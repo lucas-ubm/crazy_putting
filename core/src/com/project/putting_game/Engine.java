6c3cf5d1cb586d10263e33a172730320beebf18c
Ahmad <ahmad-nl@hotmail.com>
2018-03-22 15:17:13 +0100

    Final changes

--- core/src/com/project/putting_game/Engine.java
+++ core/src/com/project/putting_game/Engine.java
@@ -1,21 +1,26 @@
+package com.project.putting_game;
 
 public class Engine
 {
 
     public static final double g = 9.81;
-    public static double friction;
+    public static double Currentfriction;
     public static final double h = 0.1;
     public static double xh;
     public static double yh;
     public static double vx_h;
     public static double vy_h;
+    public static double vx;
+    public static double vy;
+
 
     public static void Calculate(Ball ball, Field fields)
     {
-        x = ball.position.x;
-        y = ball.position.y;
-        vx = ball.velocity.x;
-        vy = ball.velocity.y;
+         double x =  ball.position.x;
+         double y =  ball.position.y;
+
+         double vx = ball.velocity.x;
+         double vy = ball.velocity.y;
 
          xh = x + h * vx;
          yh = y + h * vy;
@@ -23,17 +28,20 @@ public class Engine
          vx_h = vx + h * ForceX();
          vy_h = vy + h * ForceY();
 
-        ball(xh, yh, vx_h, vy_h);
+         ball.position.x = (float)xh;
+         ball.position.y = (float)yh;
+
+         Currentfriction = fields.matrix[(int)ball.position.x][(int)ball.position.y].friction;
     }
 
     public static double ForceX()
     {
-        double Fx = ((-g) * (xh)) - (friction * g * vx) / Math.sqrt(vx ^ 2 + vy ^ 2);
+        double Fx = ((-g) * (xh)) - (Currentfriction * g * vx) / Math.sqrt(Math.pow(vx, 2) + Math.pow(vy, 2));
         return Fx;
     }
     public static double ForceY()
     {
-        double Fy = ((-g) * (yh)) - (friction * g * vy) / Math.sqrt(vx ^ 2 + vy ^ 2);
+        double Fy = ((-g) * (yh)) - (Currentfriction * g * vy) / Math.sqrt(Math.pow(vx, 2) + Math.pow(vy, 2));;
         return Fy;
     }
 }
--- core/src/com/project/putting_game/Properties.java
+++ core/src/com/project/putting_game/Properties.java
@@ -4,9 +4,6 @@ public class Properties {
     public double height;
     public double friction;
 
-    public Properties(double height, double friction) {
-        this.height = height;
-        this.friction = friction;
-    }
+
 
 }
