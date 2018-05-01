9694b8bb3745c522607c313314e6cf2b1b7c44c5
Mavrokefalidis <lgubm23@gmail.com>
2018-05-01 12:38:08 +0200

    Getting Floor's changes

--- core/src/com/project/putting_game/Field.java
+++ core/src/com/project/putting_game/Field.java
@@ -13,47 +13,20 @@ public class Field {
         for(int i = 0; i < matrix.length; i++) {
             for(int j = 0; j < matrix[0].length; j++) {
                 matrix[i][j] = new Properties();
-            }
-        }
-
-
-        if(course.equals("sinx+siny")) {
-            for(int i = 0; i < matrix.length; i++) {
-                for(int j = 0; j < matrix[0].length; j++) {
-                    matrix[i][j].height = Math.sin((double)(j)/(400/5.1))+Math.sin((double)(i)/(240/5.1));
-                }
-            }
-        }
-        else if(course.equals("flat")) {
-            for(int i = 0; i < matrix.length; i++) {
-                for(int j = 0; j < matrix[0].length; j++) {
+                if(course.equals("flat")){
                     matrix[i][j].height = 1;
                 }
-            }
-        }
-        else if(course.equals("slope")) {
-            for(int i = 0; i < matrix.length; i++) {
-                for(int j = 0; j < matrix[0].length; j++) {
-                    matrix[i][j].height = i+j;
+                else if(course.equals("slope")||course.equals("parabola")){
+                    matrix[i][j].height = i + j;
                 }
-            }
-        }
-        else if(course.equals("parabola")) {
-            for(int i = 0; i < matrix.length; i++) {
-                for(int j = 0; j < matrix[0].length; j++) {
-                    matrix[i][j].height = i+j;
+                else if(course.equals("sinx+siny")){
+                    matrix[i][j].height = Math.sin((double)(j)/(400/5.1))+Math.sin((double)(i)/(240/5.1));
                 }
-            }
-        }
-
-        for(int i = 0; i < matrix.length; i++) {
-            for(int j = 0; j < matrix[0].length; j++) {
                 matrix[i][j].friction = 0.4;
             }
         }
 
         this.hole = hole;
         this.holeRadius = holeRadius;
-
     }
 }
--- core/src/com/project/putting_game/MenuScreen.java
+++ core/src/com/project/putting_game/MenuScreen.java
@@ -12,22 +12,18 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-
 public class MenuScreen implements Screen {
 	final com.project.putting_game.Project2 game;
-	public static boolean gameMode1 = true;
+	//public static boolean gameMode1 = true;
 	OrthographicCamera camera;
-	Skin skin;
 	private Stage stage;
 	private TextButton startButton;
 	private TextButton controlsButton;
-	TextButton.TextButtonStyle textButtonStyle;
 	private Texture golfImg;
 	private Rectangle golf;
 	private ButtonGroup radioButtons;
 	private Texture checkImg;
 	private Texture uncheckImg;
-	Label.LabelStyle labelStyle;
 	private Label mode1Label;
 	private Label mode2Label;
 	ImageButton checkBox1;
@@ -39,66 +35,53 @@ public class MenuScreen implements Screen {
 	 * Instantiating all variables defined above and its components (such as position and size).
 	 * @param game - game created when 'run' was clicked (parent of all screens)
 	 */
-	public MenuScreen(final com.project.putting_game.Project2 game){ //create()
+	public MenuScreen(final com.project.putting_game.Project2 game) { //create()
 		this.game = game;
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800,480);
+		camera.setToOrtho(false, 800, 480);
 
 		//create a nice picture to illustrate our game for the background of the MenuScreen
 		golfImg = new Texture(Gdx.files.internal("Golf.jpg"));
 		golf = new Rectangle(); //create a Rectangle which can contain the picture
 		golf.width = Gdx.graphics.getWidth(); //set it to cover the entire width of the screen
-		golf.height = Gdx.graphics.getWidth()*721/1280; //but not the entire height, just what is needed to keep the original size
+		golf.height = Gdx.graphics.getWidth() * 721 / 1280; //but not the entire height, just what is needed to keep the original size
 		golf.x = 0;
 		golf.y = Gdx.graphics.getHeight() - golf.height;
 
-		skin = new Skin();
-		skin.add("default", game.font);
-		//Create a texture
-		Pixmap backgroundButton = new Pixmap((int)Gdx.graphics.getWidth()/2,(int)Gdx.graphics.getHeight()/10,Pixmap.Format.RGB888);//format is enum: how to store color values
-		backgroundButton.setColor(Color.WHITE);
-		backgroundButton.fill();
-		skin.add("background",new Texture(backgroundButton));
-		//Create a button style
-		textButtonStyle = new TextButton.TextButtonStyle();
-		textButtonStyle.up = skin.newDrawable("background", Color.FOREST); //standard color of the button (no special action)
-		textButtonStyle.down = skin.newDrawable("background", Color.BLACK); //let the button turn black if you press your mouse when standing on the button
-		textButtonStyle.checked = skin.newDrawable("background", Color.BLACK);
-		textButtonStyle.over = skin.newDrawable("background", Color.BROWN); //let the button turn brown when the mouse is standing on the button
-		textButtonStyle.font = skin.getFont("default");
-		skin.add("default", textButtonStyle);
-		backgroundButton.dispose();//don't need it anymore, only taking memory
 		stage = new Stage(new ScreenViewport());
 		Gdx.input.setInputProcessor(stage);//let game take input from the stage
-		startButton = new TextButton("Play!", skin);
-		startButton.setPosition(Gdx.graphics.getWidth()/2 - startButton.getWidth()/2,2*Gdx.graphics.getHeight()/6);
+		startButton = new TextButton("Play!", game.skin);
+		startButton.setPosition(Gdx.graphics.getWidth() / 2 - startButton.getWidth() / 2, 2 * Gdx.graphics.getHeight() / 6);
 		//When button 'Play!' is clicked set the screen to the GameScreen (and close MenuScreen)
-		startButton.addListener(new ClickListener(){
-			public void clicked(InputEvent event,float x, float y){
-				game.setGameMode(gameMode1);
-				game.setScreen(new com.project.putting_game.Game(game));
+		startButton.addListener(new ClickListener() {
+			public void clicked(InputEvent event, float x, float y) {
+				if (checkBox2.isChecked()) {
+					game.setGameMode(false);
+					game.setScreen(new com.project.putting_game.SelectInputScreen(game));
+				} else{
+					game.setGameMode(true);
+					game.setScreen(new com.project.putting_game.Game(game));
+				}
 				dispose();
 			}
 		});
 
 		stage.addActor(startButton);
-		controlsButton = new TextButton("Controls", skin);
-		controlsButton.setPosition(Gdx.graphics.getWidth()/2 - controlsButton.getWidth()/2,Gdx.graphics.getHeight()/6);
+		controlsButton = new TextButton("Controls", game.skin);
+		controlsButton.setPosition(Gdx.graphics.getWidth() / 2 - controlsButton.getWidth() / 2, Gdx.graphics.getHeight() / 6);
 		//When button 'Controls' is clicked set the screen to the ControlsScreen (and close MenuScreen)
-		controlsButton.addListener(new ClickListener(){
-			public void clicked(InputEvent event,float x, float y){
+		controlsButton.addListener(new ClickListener() {
+			public void clicked(InputEvent event, float x, float y) {
 				game.setScreen(new com.project.putting_game.ControlsScreen(game));
 				dispose();
 			}
 		});
 		stage.addActor(controlsButton);
 		//create buttons and labels for choosing which mode to play
-		labelStyle = new Label.LabelStyle();
-		labelStyle.font = game.font;
-		mode1Label = new Label("Mode 1",labelStyle);
-		mode1Label.setPosition(Gdx.graphics.getWidth()/2-mode1Label.getWidth()/2,3*Gdx.graphics.getHeight()/6 );
-		mode2Label = new Label("Mode 2",labelStyle);
-		mode2Label.setPosition(Gdx.graphics.getWidth()/2-mode2Label.getWidth()/2,mode1Label.getY()-mode2Label.getHeight());
+		mode1Label = new Label("Mode 1", game.skin);
+		mode1Label.setPosition(Gdx.graphics.getWidth() / 2 - mode1Label.getWidth() / 2, 3 * Gdx.graphics.getHeight() / 6);
+		mode2Label = new Label("Mode 2", game.skin);
+		mode2Label.setPosition(Gdx.graphics.getWidth() / 2 - mode2Label.getWidth() / 2, mode1Label.getY() - mode2Label.getHeight());
 		checkImg = new Texture(Gdx.files.internal("checked.png"));
 		uncheckImg = new Texture(Gdx.files.internal("unchecked.png"));
 		checkedState = new TextureRegionDrawable(new TextureRegion(checkImg));
@@ -108,36 +91,14 @@ public class MenuScreen implements Screen {
 		imageButtonStyle.imageUp = uncheckedState;
 		checkBox1 = new ImageButton(imageButtonStyle);
 		checkBox2 = new ImageButton(imageButtonStyle);
-		checkBox1.setSize(mode1Label.getHeight(),mode1Label.getHeight());
-		checkBox2.setSize(mode2Label.getHeight(),mode2Label.getHeight());
-		checkBox1.setPosition(mode1Label.getX()-checkBox1.getWidth(), mode1Label.getY());
-		checkBox2.setPosition(mode2Label.getX()-checkBox2.getWidth(), mode2Label.getY());
-		checkBox1.addListener(new ClickListener(){
-			public void clicked(InputEvent event,float x, float y){
-				gameMode1 = true;
+		checkBox1.setSize(mode1Label.getHeight(), mode1Label.getHeight());
+		checkBox2.setSize(mode2Label.getHeight(), mode2Label.getHeight());
+		checkBox1.setPosition(mode1Label.getX() - checkBox1.getWidth(), mode1Label.getY());
+		checkBox2.setPosition(mode2Label.getX() - checkBox2.getWidth(), mode2Label.getY());
 
-			}
-		});
-		checkBox2.addListener(new ClickListener(){
-			public void clicked(InputEvent event,float x, float y){
-				gameMode1 = false;
-			}
-		});
-
-
-		radioButtons= new ButtonGroup();
+		radioButtons = new ButtonGroup();
 		radioButtons.add(checkBox1);
 		radioButtons.add(checkBox2);
-		checkBox1.addListener(new ClickListener(){
-			public void clicked(InputEvent event,float x, float y){
-				gameMode1 = true;
-			}
-		});
-		checkBox2.addListener(new ClickListener(){
-			public void clicked(InputEvent event,float x, float y){
-				gameMode1 = false;
-			}
-		});
 		radioButtons.setMaxCheckCount(1);
 		checkBox1.setChecked(true);
 
@@ -145,7 +106,9 @@ public class MenuScreen implements Screen {
 		stage.addActor(mode2Label);
 		stage.addActor(checkBox1);
 		stage.addActor(checkBox2);
+
 	}
+
 	/** Called many times a second. Draws all textures and elements of the stage, such as buttons and labels, on the screen.
 	 * @param delta -  time elapsed since rendering the last frame
 	 */
@@ -154,7 +117,6 @@ public class MenuScreen implements Screen {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		camera.update();
 		stage.act();
-
 		stage.getBatch().setProjectionMatrix(camera.combined);
 		stage.getBatch().begin();
 		stage.getBatch().draw(golfImg, golf.x, golf.y, golf.width, golf.height);//draw background picture
@@ -163,6 +125,7 @@ public class MenuScreen implements Screen {
 	}
 	@Override
 	public void resize(int width, int height) {
+		stage.getViewport().update(width, height);
 	}
 	@Override
 	public void show(){
