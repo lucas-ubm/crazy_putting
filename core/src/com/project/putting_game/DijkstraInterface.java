d515ee3780227ea066e5396d0ec7f088350985f9
Fadi Katrina <f.fadikatrina@gmail.com>
2018-06-19 13:09:17 +0200

    radical changes to implement an adjecency style of Dijkstra

---
+++ core/src/com/project/putting_game/DijkstraInterface.java
@@ -0,0 +1,31 @@
+public class Dijkstra {
+
+    Field course;
+    Vector3 start;
+    Vector3 target;
+    double[][] heights;
+    ArrayList miniTargets = new ArrayList();
+
+    public Dijktra (Field c, Ball ball, Hole hole){
+        course = c;
+        start = ball.getPosition;
+        target = hole.getPosition;
+        heights = translateToHeights(c.getMatrix());
+    }
+
+    public double[][] translateToHeights(Properties[][] matrix) {
+
+        double[][] heights = new double[Gdx.graphics.getHeight()][Gdx.graphics.getWidth()];
+
+        for(int i; i<Gdx.graphics.getHeight() ; i++){
+            for(int m ; m<Gdx.graphics.getWidth() ; m++){
+                heights[i][m] = matrix[i][m].getHeight();   }    }
+        return heights;
+    }
+
+    public void label(){
+        
+    }
+
+
+}
