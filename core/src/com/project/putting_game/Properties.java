9b8abe637ec33b2c74c3ac1864e3c2272e0210d8
Fadi Katrina <f.fadikatrina@gmail.com>
2018-06-19 11:53:13 +0200

    .

--- core/src/com/project/putting_game/Properties.java
+++ core/src/com/project/putting_game/Properties.java
@@ -3,9 +3,12 @@ package com.project.putting_game;
 public class Properties {
     public double height;
     public double friction;
+<<<<<<< HEAD
 
     public double getHeight() {
         return height;
     }
 
+=======
+>>>>>>> a6168aa40895fb1e5a2fd7cfaa0018372cc22e16
 }
