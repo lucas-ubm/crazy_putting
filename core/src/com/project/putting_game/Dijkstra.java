33ecf75388f755ef9d3fc78083983ceac726f35a
Fadi Katrina <f.fadikatrina@gmail.com>
2018-06-19 12:19:02 +0200

    constructor of the dijkstra object done

--- core/src/com/project/putting_game/Dijkstra.java
+++ core/src/com/project/putting_game/Dijkstra.java
@@ -1,18 +1,28 @@
 public class Dijkstra {
 
     Field course;
-    Hole target;
-    ArrayList holes = new ArrayList();
-    double[][] heights; = new double[][]
+    Vector3 start;
+    Vector3 target;
+    double[][] heights;
+    ArrayList miniTargets = new ArrayList();
 
-    public Dijktra (Field c, Hole hole){
+    public Dijktra (Field c, Ball ball, Hole hole){
         course = c;
-        target = hole;
-        heights = new double[][]
+        start = ball.getPosition;
+        target = hole.getPosition;
+        heights = translateToHeights(c.getMatrix());
     }
 
-    public void overlay() {
+    public double[][] translateToHeights(Properties[][] matrix) {
 
+        double[][] heights = new double[Gdx.graphics.getHeight()][Gdx.graphics.getWidth()];
+
+        for(int i; i<Gdx.graphics.getHeight() ; i++){
+            for(int m ; m<Gdx.graphics.getWidth() ; m++){
+                heights[i][m] = matrix[i][m].getHeight();   }    }
+        return heights;
     }
+
+
 
 }
