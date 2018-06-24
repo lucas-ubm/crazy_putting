5b8a80a12154bde1fbe6f3f057c0eb0be7383459
Fadi Katrina <f.fadikatrina@gmail.com>
2018-06-24 12:57:36 +0200

    all noise + testing main

---
+++ core/src/com/project/putting_game/Noise/NoiseMachine.java
@@ -0,0 +1,12 @@
+
+public interface NoiseMachine{
+    
+    double speedLimit = 5;
+    double[] dRL = {0, 5};
+
+    //contructor: two arguments, double represents the speed limit, double[2] represents the direction degree limits
+    //if constructed without arguments: default are 5, and 0-360.
+    //return: [0] represents the speed, [1] represents the direction of noise
+    public double[] generateNoise();
+
+}
